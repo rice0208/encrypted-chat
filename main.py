d2fc39cde45176f201de7dca5b76d3c75f98c02a
rice0208 <riceforever0208@outlook.com>
2022-04-08 19:52:51 +0800

    format main.py with black

--- main.py
+++ main.py
@@ -2,26 +2,29 @@ import base64
 import pyDes as pd
 import random
 
+
 class Encrypt:
     def __init__(self, des_key, des_iv):
         self.des_key = des_key
         self.des_iv = des_iv
+
     def encrypt(self, s):
         k = pd.des(self.des_key, pd.CBC, self.des_iv, pad=None, padmode=pd.PAD_PKCS5)
         es = k.encrypt(s)
         return base64.b64encode(es).decode()
+
     def decrypt(self, s):
         s = base64.b64decode(s)
         k = pd.des(self.des_key, pd.CBC, self.des_iv, pad=None, padmode=pd.PAD_PKCS5)
         ds = k.decrypt(s, padmode=pd.PAD_PKCS5)
         return ds.decode()
 
+
 def get_key_from_exchange(p):
     a = random.randint(960, 1460)
-    print("[Send] %s" % str((g ** a) % p))
+    print("[Send] %s" % str((g**a) % p))
     g_b = int(str(input("Type what your target sent.\n[Input] ")))
-    return (g_b ** a) % p
-
+    return (g_b**a) % p
 
 
 try:
@@ -37,7 +40,11 @@ try:
         exit(0)
     while True:
         try:
-            option = int(input("Enter your option. 1 for encrypting, 2 for decrypting.\n[Option] "))
+            option = int(
+                input(
+                    "Enter your option. 1 for encrypting, 2 for decrypting.\n[Option] "
+                )
+            )
             if option not in [1, 2]:
                 print("\n[Warning] Invalid option.\n")
                 continue
